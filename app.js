503da38dab521ac402675077069b72d1fc43af60
nonogon <leslie.strauss@gmail.com>
2014-10-15 19:24:16 -0700

    Using model for hare.

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ App.Router.map(function() {
   this.route('seaturtle');
   this.route('snappingturtle');
 });
+
+App.HareRoute = Ember.Route.extend ({
+  model: function() {
+    return {
+      turtle: "Hare",
+      url: "https://1.bp.blogspot.com/-_hR7ja4ElGo/UQsBaesAPPI/AAAAAAAARz4/LgFBJ4n4Sk4/s1600/hare%252520web-0184.jpg"
+    };
+  }
+})
